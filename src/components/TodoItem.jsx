1fa2c8c3ee880b16145b73747aa639222cdb9f00
damarwu <hello.damarwu@gmail.com>
2024-06-03 11:40:27 +0700

    Develop delete todo feature

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-const TodoItem = ({todo, toggleCompleted}) => {
+const TodoItem = ({todo, toggleCompleted, deleteTodo}) => {
     const getTodoTitleStyle = () => {
         if (todo.completed === true) {
             return {textDecoration: 'line-through'}
@@ -11,8 +11,14 @@ const TodoItem = ({todo, toggleCompleted}) => {
 
     return (
     <div style={styles.todoItem}>
-        <input type="checkbox" style={styles.checkbox} onChange={() => toggleCompleted(todo.id)} />
+        <input 
+            type="checkbox" 
+            style={styles.checkbox} 
+            onChange={() => toggleCompleted(todo.id)} />
         <p style={getTodoTitleStyle()}>{todo.title}</p>
+        <button 
+            style={styles.button}
+            onClick={() => deleteTodo(todo.id)}>x</button>
     </div>
     )
 
@@ -23,14 +29,25 @@ const styles = {
         border: '2px solid #f4f4f4',
         fontSize: '24px',
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
+        padding: '0 20px'
     },
     checkbox: {
         marginRight: '10px',
         height: '18px',
         width: '18px',
-    }
+    },
+    button: {
+        backgroundColor: '#BB0000',
+        color: '#fff',
+        height: '30px',
+        width: '30px',
+        borderRadius: '100%',
+        border: 'none',
+        cursor: 'pointer',
+        fontSize: '16px',
+    },
 }
 
 export default TodoItem
